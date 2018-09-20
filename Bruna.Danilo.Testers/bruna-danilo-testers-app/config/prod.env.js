278150cb36127e078955b0b4ea76de6137a64605
Danilo Andrade Oliveira <daniloao@gmail.com>
2018-09-20 19:54:01 +0100

    test

--- Bruna.Danilo.Testers/bruna-danilo-testers-app/config/prod.env.js
+++ Bruna.Danilo.Testers/bruna-danilo-testers-app/config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
     NODE_ENV: '"production"',
-    API_URL_ADDRESS: '"http://localhost:26116/api/"',
+    API_URL_ADDRESS: '"https://bruna-danilo-testers-api.azurewebsites.net/api/"',
     DEFAULT_PAGE_SIZE: 15
 }
